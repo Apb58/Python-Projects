5e9e74c72130a53e0aae3b9d4ced2ef748e15908
Adrian Bubie <Apb58@users.noreply.github.com>
2017-06-19 06:35:57 -0700

    Update Gene_Validator.py
    Updated description of gene lists to be more generic; moved the comparison to a function.

--- Gene_Validator.py
+++ Gene_Validator.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 # Written by Adrian Bubie (git hub: https://github.com/Apb58)
 #Gene Name Validation checker for new test panels:
-#Checks Gene name from the list of genes from the genes index against the genes
-#listed in the test regions.
+#Checks Gene name from the list of genes 1 against the genes from list 2.
+
+def gene_comparitor(Genes, test_regions):
+    count = len(test_regions)
+    print "Genes in the Test Regions:", count
+    
+    for gene in test_regions:
+        if gene not in Genes:
+            print ("error: " + gene + " not in genes index (list 1)")
+        else:
+            print (gene + " is in genes index (list 1)")
+
 
 Gene_list = open('Genes_List.txt','r')
 Raw_Genes = Gene_list.readlines()
@@ -20,13 +30,5 @@ for item in raw_test_regions:
     item = item.replace('\n','')
     test_regions.append(item)
     
-
-for gene in test_regions:
-    if gene not in Genes:
-        print ("error: " + gene + " not in genes index")
-    else:
-        print (gene + " is in genes index")
-
-count = len(test_regions)
-print "Genes in the Test Regions:", count
+gene_comparitor(Genes, test_regions)    
     
